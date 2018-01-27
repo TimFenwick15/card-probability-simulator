6ffa63ce44f803be3f16d7a3e70f87d9b9d02486
TimFenwick15 <tjf20@hotmail.co.uk>
2018-01-27 18:21:33 +0000

    modified package.json for heroku

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static('./src'))
 app.use(bodyParser.json())
-app.listen(8080, () => console.log('Listening on port 8080'))
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on ${port}`));
+
 const deck = require('./deck')
 
 app.post('/simulate', (req, res) => {
