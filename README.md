f7bc72ac8ea2c2d20334f23f8e09e757a8f41967
TimFenwick15 <tjf20@hotmail.co.uk>
2018-01-27 18:16:36 +0000

    interface for running

---
+++ README.md
@@ -0,0 +1,1 @@
+consider having a property that determines failure rate, or condition of hand and sometimes allow this to happen
--- src/script.js
+++ src/script.js
@@ -1,42 +1,51 @@
-const deck = {
-  debug: false,
-  deckSize: 40,
-  cardsToDraw: 5,
-  targets: {
-    ABC: {
-      quantity: 9
+'use strict'
+const makeDeckObject = () => {
+  const deckSize = Number(document.getElementById('deckSize').value) || 0
+  const cardsToDraw = Number(document.getElementById('cardsToDraw').value) || 0
+  const target1 = Number(document.getElementById('target1').value) || 0
+  const target2 = Number(document.getElementById('target2').value) || 0
+  const upstart = Number(document.getElementById('upstart').value) || 0
+  const desires = Number(document.getElementById('desires').value) || 0
+  const greed = Number(document.getElementById('greed').value) || 0
+  return {
+    debug: false,
+    deckSize: deckSize,
+    cardsToDraw: cardsToDraw,
+    targets: {
+      Target1: {
+        quantity: target1
+      },
+      Target2: {
+        quantity: target2
+      }
     },
-    Hanger: {
-      quantity: 6
+    modifiers : {
+      Upstart: {
+        quantity: upstart,
+        adds: [],
+        draws: 1,
+        destroys: 0,
+        timesAllowedPerTurn: 3
+      },
+      Desires: {
+        quantity: desires,
+        adds: [],
+        draws: 2,
+        destroys: 10,
+        timesAllowedPerTurn: 1
+      },
+      Greed: {
+        quantity: greed,
+        adds: [],
+        draws: 2,
+        destroys: 0,
+        timesAllowedPerTurn: 3
+      }
     }
-  },
-  modifiers : {
-    /*Upstart: {
-      quantity: 1,
-      adds: [],
-      draws: 1,
-      destroys: 0,
-      timesAllowedPerTurn: 1
-    },*/
-    Desires: {
-      quantity: 3,
-      adds: [],
-      draws: 2,
-      destroys: 10,
-      timesAllowedPerTurn: 1
-    },
-    /*Greed: {
-      quantity: 3,
-      adds: [],
-      draws: 2,
-      destroys: 0,
-      timesAllowedPerTurn: 3
-    }*/
   }
 }
-
 const render = result => {
-  document.getElementById('result').innerHTML = result
+  document.getElementById('result').innerHTML = `${Number(result) * 100}%`
 }
 const run = () => {
   fetch('/simulate', {
@@ -44,10 +53,8 @@ const run = () => {
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(deck)
+    body: JSON.stringify(makeDeckObject())
   })
     .then(res => res.text())
     .then(res => render(res))
 }
-
-// consider having a property that determines failure rate, or condition of hand and sometimes allow this to happen
